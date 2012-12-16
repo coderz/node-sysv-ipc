088028ac8c682164100ffaeb64f7e6edbd049808
yawnt <yawn.localhost@gmail.com>
2012-12-16 20:29:34 +0100

    [minor] fix

--- build/Release/.deps/Release/msgget.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/msgget.node := ln -f "Release/obj.target/msgget.node" "Release/msgget.node" 2>/dev/null || (rm -rf "Release/msgget.node" && cp -af "Release/obj.target/msgget.node" "Release/msgget.node")
--- build/Release/.deps/Release/obj.target/msgget.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/msgget.node := flock ./Release/linker.lock g++ -shared -pthread -rdynamic -m64  -Wl,-soname=msgget.node -o Release/obj.target/msgget.node -Wl,--start-group Release/obj.target/msgget/src/msgget.o -Wl,--end-group 
